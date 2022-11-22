654b779cfaa9b6909c2964de0a6e5f05ca668fbf
elainesommerville <elaine.sommerville@instil.co>
2022-11-22 15:20:23 +0000

    submit button fix

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="" action="#" method="post" @submit.prevent="onSubmit">
+  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
     <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
     <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
     <button type="submit" name="button">Add Book</button>
