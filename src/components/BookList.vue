88b2ccca875607065a30eb7bf31250f22b2ad426
elainesommerville <elaine.sommerville@instil.co>
2022-11-22 11:57:13 +0000

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'All Books'
+    }
+  },
+  name: 'BookList'
+}
+</script>
+
+<style>
+h1 {
+  font-weight: normal;
+}
+h2 {
+  font-weight: normal;
+}
+</style>
